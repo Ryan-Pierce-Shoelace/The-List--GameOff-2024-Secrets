13a53dc2589d2f30ecf41864a2bb03fb4aa70d2c
Travis Pierce <46234513+XuulMedia@users.noreply.github.com>
2024-11-17 13:50:15 -0700

    added the ability to set random scary text on the list

--- Assets/_Scripts/Chores/DayPlanSO.cs
+++ Assets/_Scripts/Chores/DayPlanSO.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
+using Horror.Chores.HorrorEffect;
 using UnityEngine;
+using Utilities;
 
 namespace Horror.Chores
 {
@@ -8,7 +11,11 @@ namespace Horror.Chores
 	{
 		public string DayName;
 		public List<ChoreDataSO> Chores;
-		
+		public Dictionary<ChoreDataSO, HorrorEffectData> DailyHorrorEffects = new Dictionary<ChoreDataSO, HorrorEffectData>();
+
+		[SerializeField] private UnitySerializedDictionary<ChoreDataSO, HorrorTextInfo> horrorChoreText = new UnitySerializedDictionary<ChoreDataSO, HorrorTextInfo>();
+
+
 		public void ResetAllChores()
 		{
 			foreach (ChoreDataSO chore in Chores)
@@ -16,11 +23,32 @@ namespace Horror.Chores
 				chore.Reset();
 			}
 		}
-		
+
 		private void OnEnable()
 		{
 			ResetAllChores();
+			CreateHorrorEffects();
 		}
 
+
+		private void CreateHorrorEffects()
+		{
+			DailyHorrorEffects.Clear();
+
+			foreach (KeyValuePair<ChoreDataSO, HorrorTextInfo> kvp in horrorChoreText)
+			{
+				HorrorEffectData scaryData = new HorrorEffectData(kvp.Value.ScaryText, kvp.Value.Duration, kvp.Value.LikelyHood);
+
+				DailyHorrorEffects.Add(kvp.Key, scaryData);
+			}
+		}
+
+		[Serializable]
+		public struct HorrorTextInfo
+		{
+			public string ScaryText;
+			public float Duration;
+			public float LikelyHood;
+		}
 	}
 }
